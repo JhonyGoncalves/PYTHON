c45729e754b53c7ee21e5b4e359f138b370e3593
JhonyGoncalves <jhony136@outlook.com>
2024-09-16 23:03:41 -0300

    .

---
+++ VARIAVEIS E OPERAÇÕES/ex008.py
@@ -0,0 +1,3 @@
+# Escreva um programa que leia um número inteiro e exiba seu sucessor e antecessor.
+n1 = int(input("Digite um número inteiro: "))
+print(f"O antecessor de {n1} é {n1 - 1} e o sucessor é {n1 + 1}")
