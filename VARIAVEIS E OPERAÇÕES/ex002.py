7dc291cd0f51395c3881302a8bf6c7a65f223c29
JhonyGoncalves <jhony136@outlook.com>
2024-09-16 22:20:56 -0300

    .

---
+++ VARIAVEIS E OPERAÇÕES/ex002.py
@@ -0,0 +1,4 @@
+# Escreva um programa que leia o valor do raio de um círculo e calcule sua área.
+raio = float(input("Qual o raio do circulo?: "))
+area = raio * raio
+print(f"A área do circulo de raio {raio} é {area}")
