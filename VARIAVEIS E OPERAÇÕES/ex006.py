b41fe36fe18ae4c4048b8c573d7bc61a9efbaad8
JhonyGoncalves <jhony136@outlook.com>
2024-09-16 22:56:00 -0300

    .

---
+++ VARIAVEIS E OPERAÇÕES/ex006.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia dois números e exiba a média entre eles.
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro: "))
+media = (n1 + n2) / 2
+print(f"A media entre {n1} e {n2} é {media}")
