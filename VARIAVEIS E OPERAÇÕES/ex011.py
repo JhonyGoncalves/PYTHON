a03ca642a50581ec2fdda067cb5d87dd4f421728
JhonyGoncalves <jhony136@outlook.com>
2024-09-16 23:19:03 -0300

    .

---
+++ VARIAVEIS E OPERAÇÕES/ex011.py
@@ -0,0 +1,3 @@
+# Escreva um programa que leia o valor de um produto e calcule o valor das parcelas em 5 vezes sem juros.
+produto = float(input("Digite o valor do produto: "))
+print(F"R${produto} ou em 5x de R${produto / 5} sem juros")
