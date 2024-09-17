816d847c5ad84ecbebf6b8c5793f888f69bbd193
JhonyGoncalves <jhony136@outlook.com>
2024-09-16 22:14:50 -0300

    first commit

---
+++ VARIAVEIS E OPERAÇÕES/ex001.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia dois números e exiba a soma deles.
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite outro numero: "))
+soma = n1 + n2
+print(f"A soma entre {n1} e {n2} é igual a {soma}")
