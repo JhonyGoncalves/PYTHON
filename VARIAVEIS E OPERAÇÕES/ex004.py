92b86f9062a67fb62f6ce0c10644a17a71cdee71
JhonyGoncalves <jhony136@outlook.com>
2024-09-16 22:34:13 -0300

    .

---
+++ VARIAVEIS E OPERAÇÕES/ex004.py
@@ -0,0 +1,4 @@
+# Escreva um programa que converta metros para centímetros.
+metro = float(input("Digite um valor em metros: "))
+centimetro = metro / 100
+print(f"{metro} metros para centimetros são {centimetro}")
