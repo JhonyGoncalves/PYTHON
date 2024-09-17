92bf77c58011063a955a98477e03976817b0b1e0
JhonyGoncalves <jhony136@outlook.com>
2024-09-16 23:01:11 -0300

    .

---
+++ VARIAVEIS E OPERAÇÕES/ex007.py
@@ -0,0 +1,4 @@
+# Faça um programa que converta uma temperatura de Celsius para Fahrenheit.
+celsius = float(input("Quantos graus celsius está no seu local: "))
+fahrenheit = (celsius * 9 / 5) + 32
+print(f"{celsius}°C são {fahrenheit}°F")
