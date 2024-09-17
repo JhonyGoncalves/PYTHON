f84039bd82d26bf8eb80d051f0b4ab75107e7e7d
JhonyGoncalves <jhony136@outlook.com>
2024-09-16 22:37:22 -0300

    .

---
+++ VARIAVEIS E OPERAÇÕES/ex005.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia a idade de uma pessoa e exiba quantos dias ela já viveu.
+idade = int(input("Digite sua idade: "))
+dias = idade * 365
+print(f"Com {idade} anos você já viveu aproximadamente {dias} dias")
